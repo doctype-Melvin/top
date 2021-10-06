a1ec1ec4d1861e86555b1ca3fa9f5696cc6ac704
doctype-Melvin <melvin.speckamp@gmail.com>
2021-10-06 15:49:29 +0200

    first try at RPS

--- jstut/rock-paper-scissors/script.js
+++ jstut/rock-paper-scissors/script.js
@@ -1,8 +1,17 @@
 //The computer's choice function
+let choice = ['Rock', 'Paper', 'Scissors'];
 function computerPlay() {
-   let choice = ['Rock', 'Paper', 'Scissors'];
-   return choice[Math.floor(Math.random() * choice.length)];
+   return choice[Math.floor(Math.random()*choice.length)];
 }
+
+//Automated player function
+function playerPlay() {
+    return choice[Math.floor(Math.random()*choice.length)];
+}
+
+//Scores
+let compScore = 0
+let playScore = 0
 
 //The play round function
 function playRound(playerSelection, computerSelection) {
@@ -10,21 +19,27 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
         (playerSelection == 'Paper' && computerSelection == 'Rock') ||
         (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+            playScore++
         return `Player wins! ${playerSelection} beats ${computerSelection}!`
     }else if (playerSelection == computerSelection) {
             return `Tied game! Both chose ${playerSelection}.`
-        }       return `Computer wins! ${computerSelection} beats ${playerSelection}!`
+        }     compScore++
+                return `Computer wins! ${computerSelection} beats ${playerSelection}!`
 }
+
+
 
 //The game function
 
-function game() {
-    playRound()        
-}
-    
-    
 
-let computerSelection = computerPlay().toLowerCase();
-let playerSelection = 'Scissors'.toLowerCase();
+
+  
+
+let computerSelection = computerPlay();
+let playerSelection = playerPlay();
+
 console.log(computerSelection)
+console.log(playerSelection)
 console.log(playRound(playerSelection, computerSelection))
+console.log(playScore)
+console.log(compScore)
