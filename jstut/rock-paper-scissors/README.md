97e4fd5ab32c3e553bb105afec0046c1a75a02a9
doctype-Melvin <melvin.speckamp@gmail.com>
2021-10-21 06:14:51 +0200

    ui for rps

--- jstut/rock-paper-scissors/README.md
+++ jstut/rock-paper-scissors/README.md
@@ -1,4 +1,4 @@
-10/08, Friday
+Oct. 08, Friday
 After three days I finally accomplished the automated version
 of the Rock Paper Scissors challenge. This console based game
 now uses functions for the decision process, has a function which contains 
@@ -25,3 +25,38 @@ for the next round. The final winner declaration and score is displayed.
 
 Next: If the user clicks cancel, the auto function should play the game or the 
 game should be canceled.
+
+Oct. 15, Friday
+Starting with adding a UI to the project. 
+
+Oct. 19, Tuesday
+What a battle this has been. I couldn't come up with a solution myself.
+Did a bit of digging and found some code that helped
+me big time. The functionality is now implemented
+but I'm really struggling with the DOM 
+manipulation part. Once I finished this assisted version, 
+I will have to redo everything again. Repetition is key to learning.
+
+Oct. 20, Wednesday
+Yesterday I started the whole RPS project from scratch. 
+I started out with assembling an HTML scaffolding followed by CSS styling.
+Just today I rewrote the whole game logic in JS. 
+I ended up adding more functionality than last time.
+Points added:
+-Scorebord with initial '0:0' value
+-flex-grow properties (beta)
+-clickable pictures (player input)
+-animation when picture is clicked
+-DOM manipulation (round winner and game winner declaration)
+-Play again button that restarts the game
+
+Next step: Modify code to add/remove classes for the computer choice animation.
+
+Oct. 21, Thursday
+Within the first hour of todays session I found a solution to my problem 
+(with the help of the interwebs that is). My Problem was: The computer 
+selection animation would only be triggered once (classList.add) or toggled (classList.toggle). 
+I wanted to trigger the animation each round of playing the game. The solution involves triggering DOM reflow. 
+What this does is it updates the elements properties and hence reinitializes them. The 'void' keyword let's me trigger this process.
+To make the animation start over again each round, I simply used this flow: remove class >>> void (reflow using 'offsetWidth') >>> add class.
+The whole rock paper scissors project will rest for now. I think I made some real progress and even had some big leaps in better understanding the JS syntax. I surely will come back later to this project but for now I feel like I have to move on.
--- jstut/rock-paper-scissors/script.js
+++ jstut/rock-paper-scissors/script.js
@@ -1,95 +1,104 @@
-//Choice Array
-let choice = ['rock', 'paper', 'scissors'];
+let roundsPlayed = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-//Player and computer functions
-    //This function is the automated playerChoice function
-/*function playerChoice() {
+let choice = ['rock', 'paper', 'scissors']
+
+function cpu(){
     return choice[Math.floor(Math.random()*choice.length)];
-}
-*/
-//This function gets player input to play one round
-function playerChoice() {
-    let playerWeapon = prompt(`Your weapon of choice.
-        1 = Rock    2 = Paper    3 = Scissors
-        Or just hit OK for random attack`);
-        if (playerWeapon == 1) {
-            return choice[0];            
-        }else if (playerWeapon == 2) {
-            return choice[1];
-        }else if (playerWeapon == 3) {
-            return choice[2];
-        }else if (playerWeapon == "") { //If user only clicks OK the random function picks weapon
-            return choice[Math.floor(Math.random()*choice.length)];
-        }
-}
+};
 
-//This is the computer function
-function computerChoice() {
-    return choice[Math.floor(Math.random()*choice.length)];
-}
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
 
-//Player and computer variables
-let player
-let computer 
+rock.addEventListener('click', () => {game('rock')});
+paper.addEventListener('click', () => {game('paper')});
+scissors.addEventListener('click', () => {game('scissors')});
 
-//Counters
-let playerScore = 0
-let computerScore = 0
-let roundsPlayed = 0
+let userScore = document.querySelector('.player-score');
+let cpuScore = document.querySelector('.computer-score');
+let round = document.querySelector('.round-result');
+let winner = document.querySelector('.game-winner');
 
-//The play round function
-function playRound() {
-    //Rock, paper or scissors choice functions
-    player = playerChoice();
-        console.log(player);
-    computer = computerChoice();
-        console.log(computer);
-
-    //Winning cases
+function playRound(ply, comp) {
     roundsPlayed++
-    if ((player == 'rock' && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'paper') ||
-        (player == 'paper' && computer == 'rock')) {
+    if ((ply == 'rock' && comp == 'scissors') ||
+        (ply == 'paper' && comp == 'rock') ||
+        (ply == 'scissors' && comp == 'paper')) {
             playerScore++;
-            return alert(`Player wins! 
-                    Score Player: ${playerScore} vs Computer: ${computerScore}`)
-        }else if (player == computer) {
-            return alert(`It's a draw!
-                    Score Player: ${playerScore} vs Computer: ${computerScore}`)
-        }   computerScore++
-            return alert(`Computer wins!
-                    Score Player: ${playerScore} vs Computer: ${computerScore}`)
+            userScore.textContent = `${playerScore}`;
+            round.textContent = `${ply} beats ${comp}`;
+            return
+                }else if (ply == comp) {
+                    round.textContent = `${ply} & ${comp}`;
+                    return 
+                }computerScore++;
+                cpuScore.textContent = `${computerScore}`;
+                round.textContent = `${ply} loses to ${comp}`;
+                    return 
 };
 
 
-//The play function
-function game(){
-    //Loop for 5 rounds
-    do {
-        playRound();
-        console.log(playerScore);
-        console.log(computerScore);
-        console.log('ROUND');
-    } while (roundsPlayed < 5);
+function game(input) {
     
-    //Final score declaration
-    if (playerScore > computerScore){
-            return alert(`Player has won the game!
-                            Final Score 
-            Player: ${playerScore} vs Computer: ${computerScore}`)
-        }else if (playerScore < computerScore){
-                return alert(`Computer has won the game!
-                                Final Score 
-                Player: ${playerScore} vs Computer: ${computerScore}`)
-                    }return alert(`Tied score! It's a draw!
-                                    Final Score 
-                    Player: ${playerScore} vs Computer: ${computerScore}`)
+    let player = input;
+    let computer = cpu();
+        showCpu(computer);
+        playRound(player, computer);
+            if (playerScore == 5) {
+                winner.innerHTML = 'You won!';
+                endGame();
+                return 
+                    }else if (computerScore == 5) {
+                        winner.innerHTML = 'Computer won!';
+                        endGame();
+                        return 
+                    }
     
 };
-console.log(game());
 
-//Console.log
-console.log('PL   CPU   RDS')
-console.log(playerScore)
-console.log(computerScore)
-console.log(roundsPlayed)
+function endGame() {
+    rock.setAttribute('disabled', 1);
+    paper.setAttribute('disabled', 1);
+    scissors.setAttribute('disabled', 1);
+        let btn = document.createElement('button');
+        btn.innerHTML = 'Play again';
+        document.querySelector('.prompt').appendChild(btn);
+        btn.addEventListener('click', () => {
+            playerScore = 0;
+            computerScore = 0;
+            userScore.textContent = `${playerScore}`;
+            cpuScore.textContent = `${computerScore}`;
+            round.textContent = `May the best take all the splendor`;
+            winner.textContent = '';
+                rock.removeAttribute('disabled');
+                paper.removeAttribute('disabled');
+                scissors.removeAttribute('disabled');
+                document.querySelector('.prompt').removeChild(btn);
+        });
+}
+
+let cpur = document.getElementById('cpur');
+let cpup = document.getElementById('cpup');
+let cpus = document.getElementById('cpus');
+
+
+function showCpu(param) {
+    let highlight = param;
+    if (highlight == 'rock') {
+        cpur.classList.remove('cpu-selection');
+        void cpur.offsetWidth;
+        cpur.classList.add('cpu-selection');
+            }else if (highlight == 'paper') {
+                cpup.classList.remove('cpu-selection');
+                void cpup.offsetWidth;
+                cpup.classList.add('cpu-selection');
+                }else if (highlight == 'scissors') {
+                    cpus.classList.remove('cpu-selection');
+                    void cpus.offsetWidth;
+                    cpus.classList.add('cpu-selection');
+                }
+        
+};
+//Toggle on/off each round???
